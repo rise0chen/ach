d258fc584f2855c24cf8bc9dbd193225026717ef
rise0chen <rise0chen@163.com>
2022-02-18 13:32:11 +0800

    ach

--- ach/src/lib.rs
+++ ach/src/lib.rs
@@ -1,6 +1,8 @@
 pub use ach_array as array;
 pub use ach_cell as cell;
+pub use ach_lazy as lazy;
 pub use ach_mpmc as mpmc;
+pub use ach_once as once;
 pub use ach_pubsub as pubsub;
 pub use ach_ring as ring;
 pub use ach_spsc as spsc;
