0b357b5615435b5f87676d4ef6d8c39eb6f5a661
rise0chen <rise0chen@163.com>
2022-06-28 17:37:13 +0800

    fix: remove Ref of subscribe

--- ach-pubsub/src/lib.rs
+++ ach-pubsub/src/lib.rs
@@ -5,18 +5,25 @@ pub use ach_array::Ref;
 use ach_ring::Ring;
 use util::Error;
 
-pub struct Subscriber<T, const N: usize>(Ring<T, N>);
-impl<T, const N: usize> Subscriber<T, N> {
+pub struct Subscriber<'a, T, const N: usize> {
+    ch: Ref<'a, Ring<T, N>>,
+}
+impl<'a, T, const N: usize> Subscriber<'a, T, N> {
     /// Removes the first element and returns it.
     ///
     /// Returns Err if the Ring is empty.
     pub fn try_recv(&self) -> Result<T, Error<()>> {
-        self.0.pop()
+        self.ch.pop()
+    }
+}
+impl<'a, T, const N: usize> Drop for Subscriber<'a, T, N> {
+    fn drop(&mut self) {
+        self.ch.remove();
     }
 }
 
 pub struct Publisher<T, const NT: usize, const NS: usize> {
-    subscribers: Array<Subscriber<T, NT>, NS>,
+    subscribers: Array<Ring<T, NT>, NS>,
     strict: bool,
 }
 impl<T, const NT: usize, const NS: usize> Publisher<T, NT, NS> {
@@ -27,11 +34,11 @@ impl<T, const NT: usize, const NS: usize> Publisher<T, NT, NS> {
             strict,
         }
     }
-    pub fn subscribe(&self) -> Option<Ref<Subscriber<T, NT>>> {
-        let subscriber = Subscriber(Ring::new());
+    pub fn subscribe(&self) -> Option<Subscriber<T, NT>> {
+        let subscriber = Ring::new();
         if let Ok(i) = self.subscribers.push(subscriber) {
             let sub = self.subscribers[i].get().unwrap();
-            Some(sub)
+            Some(Subscriber { ch: sub })
         } else {
             None
         }
@@ -45,17 +52,12 @@ impl<T: Clone, const NT: usize, const NS: usize> Publisher<T, NT, NS> {
         let mut success: usize = 0;
         let mut send = None;
         for sub in self.subscribers.iter(self.strict) {
-            if sub.ref_num() == Ok(1) {
-                // No subscriber
-                sub.remove();
-                continue;
-            }
             let value = if let Some(v) = send.take() {
                 v
             } else {
                 val.clone()
             };
-            if let Err(v) = sub.0.push(value) {
+            if let Err(v) = sub.push(value) {
                 send = Some(v.input);
             } else {
                 success += 1
